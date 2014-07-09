a3ab277d14efa5b843ac244e8f4cc1d9f731ad90
MejiUlises <jonhylargo@gmail.com>
2014-07-09 14:10:13 -0500

    General Commit

--- plot1.R
+++ plot1.R
@@ -5,7 +5,7 @@ powerhouse <- read.table("household_power_consumption.txt", sep=";",  header=TRU
 flag <- grepl("^[1-2]/2/2007", powerhouse$Date)
 powerhouse <- powerhouse[flag,]
 
-#We could also subset like this but it's more slow as we have to cast more dates 
+#We could also subset like this but it's more slow as we have to cast more strings into dates
 #powerhouse <- subset(powerhouse, powerhouse$Date >='2007/02/1' & powerhouse$Date <='2007/02/2')
 
 #We merge the Date and Time into a single vector "DateTime" and cast it to a date format
--- plot2.R
+++ plot2.R
@@ -1,21 +1,21 @@
 #We read all the rows in the file
-powerhouse <- read.table("household_power_consumption.txt", sep=";",  header=TRUE, as.is=TRUE)
+powerhouse <- read.table("../household_power_consumption.txt", sep=";",  header=TRUE, as.is=TRUE)
 
 #We match the strings that are between 1-2 of february to subset
 flag <- grepl("^[1-2]/2/2007", powerhouse$Date)
 powerhouse <- powerhouse[flag,]
 
+#We could also subset like this but it's more slow as we have to cast more strings into dates
+#powerhouse <- subset(powerhouse, powerhouse$Date >='2007/02/1' & powerhouse$Date <='2007/02/2')
+
+#We merge the Date and Time into a single vector "DateTime" and cast it to a date format
 DateTime <- paste(powerhouse$Date, powerhouse$Time, sep=" ")
 DateTime <- strptime(DateTime, format = "%d/%m/%Y %H:%M:%S")
-#powerhouse$Date <- as.Date(powerhouse$Date, format="%d/%m/%Y")
-#powerhouse$Time <- strptime(powerhouse$Time, "%H:%M:%S")
-
-#We could also subset like this but it's more slow as we have 
-#powerhouse <- subset(powerhouse, powerhouse$Date >='2007/02/1' & powerhouse$Date <='2007/02/2')
 
 #We cast the Global_active_power feature to numeric so we can plot it correctly
 powerhouse$Global_active_power <- as.numeric(powerhouse$Global_active_power)
 
 #plot #2
-with(powerhouse, plot(DateTime, Global_active_power, type="n"))
+with(powerhouse, plot(DateTime, Global_active_power, type="l", ylab="Global Active Power (kilowatts)", xlab=""))
+
 
