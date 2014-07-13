86823a0be3377958a6f6064868879f4deb4bba50
MejiUlises <jonhylargo@gmail.com>
2014-07-12 23:30:34 -0500

    Final version

--- plot4.R
+++ plot4.R
@@ -21,6 +21,8 @@ powerhouse$Sub_metering_2 <- as.numeric(powerhouse$Sub_metering_2)
 powerhouse$Sub_metering_3 <- as.numeric(powerhouse$Sub_metering_3)
 
 #plot #4
+
+png(file="plot4.png", width=480, height=480)
 par(mfrow=c(2,2))
 with(powerhouse, plot(DateTime, Global_active_power, type="l", ylab="Global Active Power (kilowatts)", xlab=""))
 with(powerhouse, plot(DateTime, Voltage, type="l", ylab="Voltage", xlab="dateTime"))
@@ -30,5 +32,6 @@ with(powerhouse, lines(DateTime, Sub_metering_2, type="l",  col="red"))
 with(powerhouse, lines(DateTime, Sub_metering_3, type="l",  col="blue"))
 legend("topright", pch="____", col=c("black","red","blue"), legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), bty="n")
 with(powerhouse, plot(DateTime, Global_reactive_power, type="l", ylab="Global Rective Power", xlab="dateTime"))
-dev.copy(png, file="plot4.png")
 dev.off()
+#dev.copy(png, file="plot4.png")
+#dev.off()
